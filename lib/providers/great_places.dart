a50aa9ddd1baea2ad1f80aa846319e1e2d456b90
Jerry Rodriguez <jrvj01@hotmail.com>
2020-11-10 18:55:49 -0400

    298. !Storing & fetching data with SQLite

--- lib/providers/great_places.dart
+++ lib/providers/great_places.dart
@@ -12,6 +12,21 @@ class GreatPlaces with ChangeNotifier {
     return [..._items];
   }
 
+  Future<void> fetchAndSetPlaces() async {
+    final dataList = await DBHelper.getData('user_places');
+    _items = dataList
+        .map(
+          (item) => Place(
+            id: item['id'],
+            title: item['title'],
+            image: File(item['image']),
+            location: null,
+          ),
+        )
+        .toList();
+    notifyListeners();
+  }
+
   void addPlace(
     String pickedTitle,
     File pickedImage,
@@ -24,7 +39,7 @@ class GreatPlaces with ChangeNotifier {
     );
     _items.add(newPlace);
     notifyListeners();
-    DBHelper.insert('places', {
+    DBHelper.insert('user_places', {
       'id': newPlace.id,
       'title': newPlace.title,
       'image': newPlace.image.path,
